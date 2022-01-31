b9555f7a1daa3fd79af6680668ee70d031606ddd
Patrick Durias <32288282+smokycarrot@users.noreply.github.com>
2022-01-31 19:20:26 +0800

    Delete secrets.sh

--- to_do_app/secrets.sh
+++
@@ -1,4 +0,0 @@
-export MONGO_URI='mongodb+srv://p-user-6969:aJoMsy401q0xf6DX@cluster0.cyc77.mongodb.net/to-do-list?retryWrites=true&w=majority'
-export MONGO_DB_NAME='to-do-list'
-export MONGO_DB_USER='p-user-6969'
-export MONGO_DB_PW='aJoMsy401q0xf6DX'
